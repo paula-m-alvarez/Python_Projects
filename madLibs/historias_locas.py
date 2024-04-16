b310bfb11addd8a2629444369174a5322ff8641a
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-04-16 20:05:07 -0300

    First basic project - madLibs

---
+++ madLibs/historias_locas.py
@@ -0,0 +1,7 @@
+adj = input("Adjetivo: ")
+adj2 = input("Ingrese un nuevo adjetivo: ")
+sustantivo = input("Ingrese un sustantivo: ")
+
+madlib = f"Programar es tan {adj}. Pero a veces pienso que puede ser {adj2} porque a veces la cosas no funcionas porque me falta {sustantivo}"
+
+print(madlib)
